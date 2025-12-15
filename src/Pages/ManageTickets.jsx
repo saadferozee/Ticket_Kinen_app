c8c305b01ce8574e1371f19dc43a0f8032b2794b
saadferozee <saadferozee@gmail.com>
2025-12-16 02:09:50 +0600

    version-3.2.0, manage tickets admin panel fully functional, bug fixed

--- src/Pages/ManageTickets.jsx
+++ src/Pages/ManageTickets.jsx
@@ -1,59 +1,43 @@
 import React from 'react';
 import { useState } from 'react';
-import { useContext } from 'react';
-import AuthContext from '../Contexts/AuthContext';
 import Swal from 'sweetalert2';
 import useAxios from '../Hooks/useAxios';
 import { useEffect } from 'react';
 import Loading from '../Components/Loading';
 import { TbCircleCheck, TbClockExclamation, TbXboxX } from 'react-icons/tb';
 import { FaBusSimple, FaHelicopter, FaPlane, FaShip } from 'react-icons/fa6';
+import useAxiosSecure from '../Hooks/useAxiosSecure';
 
 const ManageTickets = () => {
-    const { user } = useContext(AuthContext);
     const axiosInstance = useAxios();
+    const axiosSecure = useAxiosSecure();
     const [loading, setLoading] = useState(true);
     const [tickets, setTickets] = useState([]);
 
-    const handleAccept = id => {
-        console.log('accept', id)
-    }
-    const handleReject = id => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            // if (result.isConfirmed) {
-            //     axiosInstance.delete(`https://adoptyco.vercel.app/listings/delete/${id}`)
-            //         .then(response => {
-            //             if (response.data.deletedCount == 1) {
-            //                 const filteredListings = myListings.filter(product => product?._id !== id);
-            //                 setMyListings(filteredListings);
-            //                 Swal.fire({
-            //                     title: "Deleted!",
-            //                     text: "Your product has been deleted.",
-            //                     icon: "success"
-            //                 });
-            //             }
-            //         })
-
-            //}
-        });
-
-    }
-
-    useEffect(() => {
+    const fetchTickets = () => {
         axiosInstance.get(`tickets`)
             .then(response => {
                 setTickets(response.data);
                 setLoading(false);
             })
-    }, [user, axiosInstance]);
+    }
+    const handleTicketStatus = (id, status) => {
+        console.log(id, status)
+        axiosSecure.patch(`/tickets/update/status?id=${id}&status=${status}`)
+            .then(res => {
+                Swal.fire({
+                    title: "Changed!!",
+                    text: `User Role Changed Successfully.`,
+                    icon: "success"
+                });
+                res.data.modifiedCount && fetchTickets();
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetchTickets()
+    }, []); // It's not an error, it's an warning.
 
     return (
         <div className='max-w-[1200px] min-h-[65vh] mx-auto pt-[50px]'>
@@ -92,67 +76,87 @@ const ManageTickets = () => {
                                 <tbody>
                                     {/* row 1 */}
                                     {
-                                        tickets.map(ticket => (
-                                            <tr key={ticket?._id} className=''>
-                                                {/* <td></td> */}
-                                                <td>
-                                                    <div className="font-bold w-full">
-                                                        {
-                                                            ticket?.status === 'pending' ? (
-                                                                <span><TbClockExclamation className='text-[22px] text-yellow-600' /></span>
-                                                            ) : ticket?.status === 'approved' ? (
-                                                                <span><TbCircleCheck className='text-[22px] text-green-500' /></span>
-                                                            ) : ticket?.status === 'rejected' && (
-                                                                <span><TbXboxX className='text-[22px] text-red-500' /></span>
-                                                            )
-                                                        }
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <div className="flex items-center gap-3">
-
-                                                        <div className="avatar">
-                                                            <div className="mask mask-squircle h-12 w-12">
-                                                                <img
-                                                                    src={ticket?.photoURL}
-                                                                    alt="Avatar Tailwind CSS Component" />
-                                                            </div>
-                                                        </div>
-
-                                                    </div>
-                                                </td>
-                                                <td className='w-[400px'>
-                                                    <div>
-                                                        <h3 className="text-sm opacity-85">
+                                        tickets.map(ticket =>
+                                            ticket?.availableSits > 1 && (
+                                                <tr key={ticket?._id} className=''>
+                                                    {/* <td></td> */}
+                                                    <td>
+                                                        <div className="font-bold w-full">
                                                             {
-                                                                ticket?.category === 'Bus' ? (
-                                                                    <span className='flex items-center gap-1 font-light text-[12px]'><FaBusSimple className='text-[14px] text-yellow-600' /> Bus</span>
-                                                                ) : ticket?.category === 'Launch' ? (
-                                                                    <span className='flex items-center gap-1 font-light text-[12px]'><FaShip className='text-[14px] text-green-500' /> Launch</span>
-                                                                ) : ticket?.category === 'Helicopter' ? (
-                                                                    <span className='flex items-center gap-1 font-light text-[12px]'><FaHelicopter className='text-[14px] text-green-500' /> Helicopter</span>
-                                                                ) : ticket?.category === 'Plane' && (
-                                                                    <span className='flex items-center gap-1 font-light text-[12px]'><FaPlane className='text-[14px] text-red-500' /> Plane</span>
+                                                                ticket?.status === 'pending' ? (
+                                                                    <span><TbClockExclamation className='text-[22px] text-yellow-600' /></span>
+                                                                ) : ticket?.status === 'approved' ? (
+                                                                    <span><TbCircleCheck className='text-[22px] text-green-500' /></span>
+                                                                ) : ticket?.status === 'rejected' && (
+                                                                    <span><TbXboxX className='text-[22px] text-red-500' /></span>
                                                                 )
                                                             }
-                                                        </h3>
-                                                        <div className="font-bold"><span className='text-sm font-light'>From</span> <span>{ticket?.from}</span> <span className='text-xs font-light'>to</span> <span>{ticket?.to}</span></div>
-                                                    </div>
-                                                </td>
-                                                <td className='w-[100px'><p className='flex gap-1'>{ticket.availableSits} <span>left</span></p></td>
-                                                <td>
-                                                    <p>{ticket?.price == 0 ? "Free" : `${ticket?.price}tk.`}</p>
-                                                </td>
-                                                <td><p>{ticket.time}</p></td>
-                                                <td><p>{ticket.date}</p></td>
-                                                <td className='font-bold'><p>{ticket.vendorName}</p></td>
-                                                <th className='flex justify-end gap-2.5'>
-                                                    {/* <Link to={`/update-listing/${ticket?._id}`} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] font-extralight text-[#556B2F] cursor-pointer">Edit</Link> */}
-                                                    <button type='button' onClick={() => handleAccept(ticket?._id)} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] font-extralight text-[#0A2F23] cursor-pointer">Approve</button>
-                                                    <button type='button' onClick={() => handleReject(ticket?._id)} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] font-extralight text-[#ff0000] cursor-pointer">Reject</button>
-                                                </th>
-                                            </tr>
-                                        ))
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="flex items-center gap-3">
+
+                                                            <div className="avatar">
+                                                                <div className="mask mask-squircle h-12 w-12">
+                                                                    <img
+                                                                        src={ticket?.photoURL}
+                                                                        alt="Avatar Tailwind CSS Component" />
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+                                                    </td>
+                                                    <td className='w-[400px'>
+                                                        <div>
+                                                            <h3 className="text-sm opacity-85">
+                                                                {
+                                                                    ticket?.category === 'Bus' ? (
+                                                                        <span className='flex items-center gap-1 font-light text-[12px]'><FaBusSimple className='text-[14px] text-yellow-600' /> Bus</span>
+                                                                    ) : ticket?.category === 'Launch' ? (
+                                                                        <span className='flex items-center gap-1 font-light text-[12px]'><FaShip className='text-[14px] text-green-500' /> Launch</span>
+                                                                    ) : ticket?.category === 'Helicopter' ? (
+                                                                        <span className='flex items-center gap-1 font-light text-[12px]'><FaHelicopter className='text-[14px] text-green-500' /> Helicopter</span>
+                                                                    ) : ticket?.category === 'Plane' && (
+                                                                        <span className='flex items-center gap-1 font-light text-[12px]'><FaPlane className='text-[14px] text-red-500' /> Plane</span>
+                                                                    )
+                                                                }
+                                                            </h3>
+                                                            <div className="font-bold"><span className='text-sm font-light'>From</span> <span>{ticket?.from}</span> <span className='text-xs font-light'>to</span> <span>{ticket?.to}</span></div>
+                                                        </div>
+                                                    </td>
+                                                    <td className='w-[100px'><p className='flex gap-1'>{ticket.availableSits} <span>left</span></p></td>
+                                                    <td>
+                                                        <p>{ticket?.price == 0 ? "Free" : `${ticket?.price}tk.`}</p>
+                                                    </td>
+                                                    <td><p>{ticket.time}</p></td>
+                                                    <td><p>{ticket.date}</p></td>
+                                                    <td className='font-bold'><p>{ticket.vendorName}</p></td>
+                                                    <th className='flex justify-end gap-2.5'>
+                                                        {
+                                                            ticket?.status === 'pending' ? (
+                                                                <>
+                                                                    <button type='button' onClick={() => handleTicketStatus(ticket?._id, 'approved')} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] font-light text-[#0A2F23] cursor-pointer">Approve</button>
+                                                                    <button type='button' onClick={() => handleTicketStatus(ticket?._id, 'rejected')} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] font-light text-[#ff0000] cursor-pointer">Reject</button>
+                                                                </>
+                                                            ) : (
+                                                                <div className='w-full h-12 flex justify-between items-center gap-2'>
+                                                                        {
+                                                                        ticket?.status === 'approved' ? (
+                                                                            <h2 className='w-full text-center text-green-500'>Approved</h2>
+                                                                        ) : ticket?.status === 'rejected' ? (
+                                                                            <h2 className='w-full text-center text-red-500'>Rejected</h2>
+                                                                        ) : (
+                                                                            <h2 className='w-full text-center'>{ticket?.status}</h2>
+                                                                        )
+                                                                        }
+                                                                </div>
+                                                            )
+                                                        }
+                                                    </th>
+                                                </tr>
+                                            )
+                                        )
+
                                     }
 
                                 </tbody>
